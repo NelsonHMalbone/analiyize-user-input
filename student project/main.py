b5921ce9b12c71f0871e52895fc5b73234c79b5f
Nelson Malbone <nhmalbone@icloud.com>
2024-12-09 18:34:41 -0500

    created function to clean up punctation and now shows the most used word in the sentence block and shows the word that was used

--- student project/main.py
+++ student project/main.py
@@ -1,3 +1,10 @@
+# creating a function to remove punctuation to clean the text
+def remove_punctuation(prompt):
+    punctuation = ".,!?:;'\"(){}[]"
+    for char in punctuation:
+        prompt = prompt.replace(char, "")
+    return prompt
+
 # inputs
 # gonna need a user input
 
@@ -24,6 +31,28 @@ total_sentence = (prompt.count('.') +
                   prompt.count('!') +
                   prompt.count('?'))
 
+
+# most frequant word
+"build data structure"
+word_frequecy = {}
+#list of all the words
+# word_list = prompt.lower().split() # this was used before the function was created
+word_list = remove_punctuation(prompt).lower().split()
+# ilterate over the words in block of text
+for word in word_list:
+    # if word is not in dictionary will add
+    if not word in word_frequecy:
+        word_frequecy[word] = 1
+    else:
+        # if word is in dictionary in sees word again it adds 1 to the dictionary
+        word_frequecy[word] += 1
+
+# print(word_frequecy) just shows the dictionary
+most_used = max(word_frequecy, key=word_frequecy.get)
+# print(most_used)
+
+
+
 # results
 print('Text Analysis Results:')
 #print("----------------------")
@@ -32,6 +61,12 @@ print('-' * 22)
 print(f'Total Characters: {total_chars}')
 print(f'Total Words: {total_words}')
 print(f'Total Sentences: {total_sentence}')
+
+"""
+{word_frequecy[most_used]
+using [most_used] as the key for word_frequecy value
+"""
+print(f'Most Frequent word: {most_used} (used {word_frequecy[most_used]} times)')
 
 # result will be a print statement
 """
