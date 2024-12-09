001342441b1fc12fcada1b09280b1ba9ff9191d4
Nelson Malbone <nhmalbone@icloud.com>
2024-12-09 17:37:50 -0500

    setting up the user input and results section

---
+++ student project/main.py
@@ -0,0 +1,26 @@
+# inputs
+# gonna need a user input
+
+# prompt = input("Enter a block of text for analysis:\n")
+# or
+print("Enter a block of text for analysis:")
+prompt = input()
+
+
+
+
+
+# results
+print('Text Analysis Results:')
+#print("----------------------")
+# or
+print('-' * 22)
+
+# result will be a print statement
+"""
+showing the total chars words and sentences
+most frequent word
+average word length
+and average sentence lenght
+
+"""
