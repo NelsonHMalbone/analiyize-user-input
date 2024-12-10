9252bbe843a473b473be49d04c77e90181029527
Nelson Malbone <nhmalbone@icloud.com>
2024-12-10 18:47:52 -0500

    adding a way to convert str to int

--- student project/main.py
+++ student project/main.py
@@ -2,14 +2,19 @@
 
 # input the numbers
 print('Enter a series of numbers separated by spaces')
-prompt = input()
-
+prompt = input() # is a string
 lengthOfNumbers = len(prompt.split())
+
+# coverting input to a intergers
+number_list = [int(num) for num in prompt.split()]
+
+
 
 # Results
 print("Number Analysis Results")
 print('-'*22)
 print(f"Total Numbers: {lengthOfNumbers}")
+print(f"Sum of Numbers: {sumTotal}")
 
 """
 results 
