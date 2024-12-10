a4143af6ad733d1c79bb42b9cae1e2da471f3a69
Nelson Malbone <nhmalbone@icloud.com>
2024-12-10 18:58:27 -0500

    added some notes finish off print statements next to do is the number frequency

--- student project/main.py
+++ student project/main.py
@@ -3,8 +3,10 @@
 # input the numbers
 print('Enter a series of numbers separated by spaces')
 prompt = input() # is a string
+" first time go around befroe converting to a int type"
 #lengthOfNumbers = len(prompt.split())
 
+"better way in less complex way of turing a str to a int"
 # coverting input to a intergers
 number_list = [int(num) for num in prompt.split()]
 
@@ -13,8 +15,11 @@ number_list = [int(num) for num in prompt.split()]
 # Results
 print("Number Analysis Results")
 print('-'*22)
-print(f"Total Numbers: {len(number_list)}")
-#print(f"Sum of Numbers: {}")
+print(f"The total number of entries: {len(number_list)}")
+print(f"The sum of the numbers: {sum(number_list)}")
+#print(f'The range of the numbers: {}')
+#print(f'The most frequent number: {}')
+#print(f'The average of the numbers: {}')
 
 """
 results 
