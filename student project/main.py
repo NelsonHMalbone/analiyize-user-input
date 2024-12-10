df456c111a12d15cf2c560d7fda3f4b61c87cba8
Nelson Malbone <nhmalbone@icloud.com>
2024-12-10 18:53:38 -0500

    redid the total numbers to this format

--- student project/main.py
+++ student project/main.py
@@ -3,7 +3,7 @@
 # input the numbers
 print('Enter a series of numbers separated by spaces')
 prompt = input() # is a string
-lengthOfNumbers = len(prompt.split())
+#lengthOfNumbers = len(prompt.split())
 
 # coverting input to a intergers
 number_list = [int(num) for num in prompt.split()]
@@ -13,8 +13,8 @@ number_list = [int(num) for num in prompt.split()]
 # Results
 print("Number Analysis Results")
 print('-'*22)
-print(f"Total Numbers: {lengthOfNumbers}")
-print(f"Sum of Numbers: {sumTotal}")
+print(f"Total Numbers: {len(number_list)}")
+#print(f"Sum of Numbers: {}")
 
 """
 results 
